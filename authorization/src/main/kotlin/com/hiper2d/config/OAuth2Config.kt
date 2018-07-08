8628ef096dce2d683b787baeee9edbe3c7295066
Alexey Zelenovsky <hiper2d@gmail.com>
2018-07-08 13:19:06 -0400

    Made OAuth2 server works with Postman

--- authorization/src/main/kotlin/com/hiper2d/config/OAuth2Config.kt
+++ authorization/src/main/kotlin/com/hiper2d/config/OAuth2Config.kt
@@ -3,6 +3,8 @@ package com.hiper2d.config
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
@@ -10,10 +12,26 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 class OAuth2Config @Autowired constructor(
-        private val authenticationManager: AuthenticationManager
+        private val authenticationManager: AuthenticationManager,
+        private val userDetailsService: UserDetailsService
 ): AuthorizationServerConfigurerAdapter() {
 
+    override fun configure(clients: ClientDetailsServiceConfigurer) {
+        clients
+                .inMemory()
+                .withClient("frontend")
+                .secret("awesome_secret")
+                .authorizedGrantTypes(
+                        "refresh_token",
+                        "password",
+                        "client_credentials"
+                )
+                .scopes("webclient", "mobileclient")
+    }
+
     override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
-        endpoints.authenticationManager(authenticationManager)
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
     }
 }
--- authorization/src/main/kotlin/com/hiper2d/config/SecurityConfig.kt
+++ authorization/src/main/kotlin/com/hiper2d/config/SecurityConfig.kt
@@ -3,8 +3,12 @@ package com.hiper2d.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +17,28 @@ class SecurityConfig: WebSecurityConfigurerAdapter() {
     @Bean
     override fun authenticationManager(): AuthenticationManager {
         return super.authenticationManager()
+    }
+
+    @Bean
+    override fun userDetailsService(): UserDetailsService {
+        return super.userDetailsService()
+    }
+
+    // Use no encoding provider for test purposes
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("hiper2d")
+                    .password("Qwe123")
+                    .roles("USER", "ADMIN")
+                .and()
+                .withUser("tech")
+                    .password("tech")
+                    .roles("USER")
     }
 }
