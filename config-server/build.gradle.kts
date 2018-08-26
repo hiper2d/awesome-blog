4a6f9abc165f574a3983a29fb0b91dcaeba3475c
Alexey Zelenovsky <hiper2d@gmail.com>
2018-08-26 12:05:48 -0400

    Added log4j2 to config-server application

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -17,8 +17,13 @@ tasks {
 
 tasks.findByName("build")?.finalizedBy("buildDockerImage")
 
+configurations.all {
+    exclude(module = "spring-boot-starter-logging")
+}
+
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("org.springframework.cloud:spring-cloud-config-server")
 }
