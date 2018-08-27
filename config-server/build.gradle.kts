78804506f9e03333eb6f9bd34ae7b1c0255f7b93
Alexey Zelenovsky <hiper2d@gmail.com>
2018-08-26 21:06:38 -0400

    Made logger async

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -26,4 +26,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.cloud:spring-cloud-config-server")
+    implementation("com.lmax:disruptor:3.3.6") // Log4j2 async appender
 }
