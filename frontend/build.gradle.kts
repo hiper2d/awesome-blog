4f41d1d3a9c96f382498348a02721552afa5017d
Alexey Zelenovsky <hiper2d@gmail.com>
2018-09-16 18:22:59 -0400

    Added log4j2 to the frontend application.

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,7 +1,9 @@
 import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 import org.gradle.internal.os.OperatingSystem
 
+val disruptorVersion: String by project
 val springCloudVersion: String by project
+
 val isWindows = OperatingSystem.current().name.startsWith("Windows")
 val yarnCmd = if (isWindows) "yarn.cmd" else "yarn"
 
@@ -40,7 +42,9 @@ dependencyManagement {
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.springframework.boot:spring-boot-starter-log4j2")
   implementation("org.springframework.cloud:spring-cloud-starter-config")
   implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
   implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+  implementation("com.lmax:disruptor:$disruptorVersion")
 }
